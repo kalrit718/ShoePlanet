0c273bcaca90368e4f6cca7021b476759e8df9d1
kalrit718 <kalindu.rithmal.other@gmail.com>
2022-02-02 21:21:13 +0530

    Connected the external JSON file to JS files.

--- shoe_page_scripts.js
+++ shoe_page_scripts.js
@@ -1,109 +1,14 @@
-var shoeData = { "shoes": [
-    {   
-        "id":"shoe1",
-        "name":"Pure Tone",
-        "gender":"Women",
-        "style":"Flat",
-        "sizes":[4, 4.5, 5, 5.5, 6, 6.5, 7],
-        "price":75.00,
-        "colour":"Black",
-        "description":"These chic snake print shoes are crafted from a combination of soft leather and nubuck. For ultimate versatility, they feature a heel piece that folds down - allowing them to be worn as slip-ons or mules. The simple yet sophisticated upper is teamed with a low 2cm heel and rubber outsole to help with grip, while a leather lining and sock work together with our dual density Cushion Plus™ technology to provide comfort in every step.",
-        "quickspecs": ["Black leather upper", "Heritage stitching", "Breathable leather lining", "Flexible rubber sole", "Heel height: 25mm"],
-        "picture": ["images/shoe1_0.jpg", "images/shoe1_1.jpg", "images/shoe1_2.jpg", "images/shoe1_3.jpg", "images/shoe1_4.jpg", "images/shoe1_5.jpg", "images/shoe1_6.jpg"],
-        "url":"shoes/shoe1.html"
-    },
-    {
-        "id":"shoe2",
-        "name":"Orinoco 2 Lace",
-        "gender":"Women",
-        "style":"Boot",
-        "sizes":[5, 6, 7],
-        "price":89.00,
-        "colour":"Brown",
-        "description":"Refreshing our bestselling Orinoco profile, a lace-up ankle boot that stays all day wearable. The premium dark olive leather upper stays classic and crafted while the cleated sole with 3cm heel feels durable and adds grip. Perfect to step into the season with casual style.",
-        "quickspecs": ["Upper Material: Waxy Leather", "Lining Material: Textile", "Heel Height: 3 cm", "Sole Material: TR", "Fastening Type: Lace", "Removable Insole: No", "Boot Leg Circumference: 24 cm", "Boot Leg Height: 16 cm"],
-        "picture": ["images/shoe2_0.jpg", "images/shoe2_1.jpg", "images/shoe2_2.jpg", "images/shoe2_3.jpg", "images/shoe2_4.jpg", "images/shoe2_5.jpg", "images/shoe2_6.jpg"],
-        "url":"shoes/shoe2.html"
-    },
-    {
-        "id":"shoe3",
-        "name":"Wave 2.0 Way",
-        "gender":"Men",
-        "style":"Trainer",
-        "sizes":[6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11],
-        "price":110.00,
-        "colour":"Black",
-        "description":"Innovative WaveWalk technology makes Wave 2.0 Way the perfect walking shoe, with a gently curved sole that aids natural movement and a Contoured Comfort™ footbed for all-day cushioning. Its casual aesthetic in premium black leather and recycled materials has a sporty feel and an eco-friendly focus.",
-        "quickspecs": ["Upper Material: Leather", "Lining Material: Textile", "Sole Material: Rubber", "Fastening Type: Lace", "Removable Insole: No"],
-        "picture": ["images/shoe3_0.jpg", "images/shoe3_1.jpg", "images/shoe3_2.jpg", "images/shoe3_3.jpg", "images/shoe3_4.jpg", "images/shoe3_5.jpg", "images/shoe3_6.jpg"],
-        "url":"shoes/shoe3.html"
-    },
-    {
-        "id":"shoe4",
-        "name":"Howard Wing",
-        "gender":"Men",
-        "style":"Brogue",
-        "sizes":[6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 12],
-        "price":69.00,
-        "colour":"Black",
-        "description":"For a formal shoe that’s always in style, you can’t beat the classic brogue. Howard Wing in rich black leather melds a traditional finish with contemporary comfort credentials: targeted Cushion Plus™ technology, a breathable sock, padded edges and a lightweight sole. Hidden elastic offers a snug fit that’s just right, too. Eco-friendly features include a footbed, sock and laces that utilise recycled rubber and bio-based materials.",
-        "quickspecs": ["Upper Material: Leather", "Lining Material: Hairsheep", "Sole Material: TR", "Fastening Type: Lace", "Removable Insole: No"],
-        "picture": ["images/shoe4_0.jpg", "images/shoe4_1.jpg", "images/shoe4_2.jpg", "images/shoe4_3.jpg", "images/shoe4_4.jpg", "images/shoe4_5.jpg", "images/shoe4_6.jpg"],
-        "url":"shoes/shoe4.html"
-    },
-    {
-        "id":"shoe5",
-        "name":"Home Comfort",
-        "gender":"Women",
-        "style":"Slipper",
-        "sizes":[4, 5, 6, 7, 8],
-        "price":35.00,
-        "colour":"Pink",
-        "description":"A classic slipper crafted for a feel-good fit, Home Comfort will soon become your next staple at-home style. An adjustable riptape fastening and durable sole keep feet confident and supported, and a velvety rose fabric upper offers a premium feel. Ideal for gifting.",
-        "quickspecs": ["Upper Material: Textile", "Lining Material: Synthetic Fur", "Sole Material: Mixed", "Removable Insole: No"],
-        "picture": ["images/shoe5_0.jpg", "images/shoe5_1.jpg", "images/shoe5_2.jpg", "images/shoe5_3.jpg", "images/shoe5_4.jpg", "images/shoe5_5.jpg", "images/shoe5_6.jpg"],
-        "url":"shoes/shoe5.html"
-    },
-    {
-        "id":"shoe6",
-        "name":"Violet 85 Court",
-        "gender":"Women",
-        "style":"Heel",
-        "sizes":[4, 5, 5.5, 6, 7, 8],
-        "price":89.00,
-        "colour":"Black",
-        "description":"With sumptuous leather linings and a classic toe shape, the chic Violet 85 Court delivers style and comfort on a sleek 8.5cm contemporary heel. Crafted from responsibly sourced black leather, it’s a staple that can be effortlessly dressed up or down.",
-        "quickspecs": ["Upper Material: Leather", "Lining Material: Leather", "Heel Height: 8.5 cm", "Sole Material: Resin Rubber", "Removable Insole: No"],
-        "picture": ["images/shoe6_0.jpg", "images/shoe6_1.jpg", "images/shoe6_2.jpg", "images/shoe6_3.jpg", "images/shoe6_4.jpg", "images/shoe6_5.jpg", "images/shoe6_6.jpg"],
-        "url":"shoes/shoe6.html"
-    },
-    {
-        "id":"shoe7",
-        "name":"Un Abode Go",
-        "gender":"Men",
-        "style":"Loafer",
-        "sizes":[6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 12],
-        "price":89.00,
-        "colour":"Tan",
-        "description":"This classic slip-on style from our Unstructured collection ensures unbeatable comfort with every step. The upper is teamed with a lightweight EVA and rubber sole that uses a dual density footbed and Cushion Max™ technology.",
-        "quickspecs": ["Upper Material: Leather", "Lining Material: Leather", "Sole Material: Rubber", "Removable Insole: Yes"],
-        "picture": ["images/shoe7_0.jpg", "images/shoe7_1.jpg", "images/shoe7_2.jpg", "images/shoe7_3.jpg", "images/shoe7_4.jpg", "images/shoe7_5.jpg", "images/shoe7_6.jpg"],
-        "url":"shoes/shoe7.html"
-    },
-    {
-        "id":"shoe8",
-        "name":"Baby Atlas",
-        "gender":"Baby",
-        "style":"Toddler",
-        "sizes":[2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5],
-        "price":30.00,
-        "colour":"Khaki",
-        "description":"Supporting little feet on their journey of discovery, toddler trainer Tiny Cub has a super-cute fox print and is superlight, flexible, and expertly crafted to help kids develop their gait cycle naturally. The soft yet hard-wearing khaki leather upper can take on the toughest of playtimes while the durable rubber sole gives extra grip for wobbly walkers. A breathable leather footbed keeps tiny toes cool and comfortable.",
-        "quickspecs": ["Upper Material: Textile", "Lining Material: Leather", "Sole Material: Leather", "Fastening Type: Riptape", "Trims: Embroidery"],
-        "picture": ["images/shoe8_0.jpg", "images/shoe8_1.jpg", "images/shoe8_2.jpg", "images/shoe8_3.jpg", "images/shoe8_4.jpg", "images/shoe8_5.jpg", "images/shoe8_6.jpg"],
-        "url":"shoes/shoe8.html"
+
+var shoeData; 
+
+$.ajax({
+    type:"GET",
+    url:"shoes.json",
+    async: false,
+    success: function(response){
+        shoeData = response;
     }
-]}
+});
 
 var usp = new URLSearchParams(window.location.search);
 
